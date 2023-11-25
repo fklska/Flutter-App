3679a37b627811c41fa0cec7e7221886457414e1
Вячеслав Бухтояров <fklskadelovoy@gmail.com>
2023-11-25 18:07:03 +1000

    Add change theme switchers

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,90 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class ChangeTheme extends StatefulWidget{
-  const ChangeTheme({super.key});
-
-  @override
-  State<StatefulWidget> createState() => _OnChangeTheme();
+void main(List<String> args) {
+  runApp(const App());
 }
 
-class _OnChangeTheme extends State<ChangeTheme>{
-  ThemeMode themeMode = ThemeMode.system;
-  bool light = true;
+class App extends StatefulWidget{
+const App({Key? key}) : super(key: key);
 
-  void brightChange(bool value)
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App>{
+  bool light = false;
+  bool useMaterial3 = true;
+  Color defaultMainColor = const Color.fromARGB(255, 18, 212, 44);
+  Color mainColor = const Color.fromARGB(255, 255, 255, 255);
+  bool pressed = false;
+  double sliderValue = 0;
+
+  void changeLight(bool value)
   {
     setState(() {
       light = value;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Switch(
-      value: light,
-      onChanged: brightChange
-    );
+  void changeMaterial(bool value)
+  {
+    setState(() {
+      useMaterial3 = value;
+    });
   }
-}
 
+  void setMainColorRed()
+  {
+    setState(() {
+      pressed = !pressed;
+      mainColor = pressed ? Colors.red : defaultMainColor;
+    });
+  }
 
-class TutorialHome extends StatelessWidget{
-  const TutorialHome({super.key});
+  void setBorderRadius(double value)
+  {
+    setState(() {
+      sliderValue = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: const IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: light ? Brightness.dark : Brightness.light,
+        useMaterial3: useMaterial3,
+        colorSchemeSeed: mainColor
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home Page"),
+          backgroundColor: Theme.of(context).focusColor,
         ),
-        title: const Text('Example title'),
-        backgroundColor: const Color.fromARGB(234, 34, 43, 1),
+        body: Container(
+          alignment: Alignment.topLeft,
+          decoration: BoxDecoration(
+            color: Theme.of(context).secondaryHeaderColor,
+            borderRadius: BorderRadius.circular(sliderValue)
+          ),
+          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+              onPressed: setMainColorRed,
+                icon: const Icon(Icons.circle_outlined, color: Colors.red,),
+                isSelected: pressed,
+                selectedIcon: const Icon(Icons.circle_rounded, color: Colors.red,),
+                ),
+              Switch(value: light, onChanged: changeLight),
+              Switch(value: useMaterial3, onChanged: changeMaterial),
+              Slider(value: sliderValue, onChanged: setBorderRadius, min: 0, max: 100,)
+            ],
+          ),
+        )
       ),
-      backgroundColor: const Color.fromARGB(255, 32, 31, 31),
-      body: const Center(child: ChangeTheme()),
+      debugShowCheckedModeBanner: false,
     );
   }
-}
-
-void main(List<String> args) {
-  runApp(
-     MaterialApp(
-      title: 'Fklska title',
-      home: TutorialHome(),
-      themeMode: ThemeMode.light,
-      darkTheme: ThemeData.light(),
-      ),
-  );
 }
