838c32ffbb3da9bf0a1f758f79b3794b3f561203
Вячеслав Бухтояров <fklskadelovoy@gmail.com>
2023-12-03 20:27:27 +1000

    Joke button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
 void main(List<String> args) {
   runApp(const App());
 }
@@ -14,10 +18,12 @@ const App({Key? key}) : super(key: key);
 class _AppState extends State<App>{
   bool light = false;
   bool useMaterial3 = true;
-  Color defaultMainColor = const Color.fromARGB(255, 18, 212, 44);
+  Color defaultMainColor = const Color.fromARGB(255, 255, 255, 255);
   Color mainColor = const Color.fromARGB(255, 255, 255, 255);
   bool pressed = false;
   double sliderValue = 0;
+
+  String jokes = 'Здесь будут шутки';
 
   void changeLight(bool value)
   {
@@ -33,18 +39,21 @@ class _AppState extends State<App>{
     });
   }
 
-  void setMainColorRed()
-  {
-    setState(() {
-      pressed = !pressed;
-      mainColor = pressed ? Colors.red : defaultMainColor;
-    });
-  }
-
   void setBorderRadius(double value)
   {
     setState(() {
       sliderValue = value;
+    });
+  }
+
+  void jokeResponse() async
+  { 
+    var url = Uri.parse('https://official-joke-api.appspot.com/random_joke');
+    var response = await http.get(url);
+    var jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+
+    setState(() {
+      jokes = jsonResponse['setup'] + '\n' + jsonResponse['punchline'];
     });
   }
 
@@ -54,35 +63,31 @@ class _AppState extends State<App>{
       theme: ThemeData(
         brightness: light ? Brightness.dark : Brightness.light,
         useMaterial3: useMaterial3,
-        colorSchemeSeed: mainColor
+        colorSchemeSeed: mainColor,
       ),
       home: Scaffold(
-        appBar: AppBar(
+          appBar: AppBar(
           title: const Text("Home Page"),
-          backgroundColor: Theme.of(context).focusColor,
         ),
         body: Container(
           alignment: Alignment.topLeft,
           decoration: BoxDecoration(
             color: Theme.of(context).secondaryHeaderColor,
-            borderRadius: BorderRadius.circular(sliderValue)
+            borderRadius: BorderRadius.circular(sliderValue),
           ),
-          
+          width: 500,
+          height: 700,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              IconButton(
-              onPressed: setMainColorRed,
-                icon: const Icon(Icons.circle_outlined, color: Colors.red,),
-                isSelected: pressed,
-                selectedIcon: const Icon(Icons.circle_rounded, color: Colors.red,),
-                ),
+              Text(jokes, style:  const TextStyle(fontSize: 16),),
+              IconButton(onPressed: jokeResponse, icon: const Icon(Icons.get_app, color: Colors.amber,)),
               Switch(value: light, onChanged: changeLight),
               Switch(value: useMaterial3, onChanged: changeMaterial),
               Slider(value: sliderValue, onChanged: setBorderRadius, min: 0, max: 100,)
             ],
           ),
-        )
+        ),
       ),
       debugShowCheckedModeBanner: false,
     );
